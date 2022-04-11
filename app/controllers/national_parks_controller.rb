64b4000c49e782b00ea92fba9bbf0c9b5b88fe5d
jeffspatafora <jeffspatafora@gmail.com>
2022-04-10 21:31:45 -0700

    adds rack-cors and configures cors.rb for localhost:8080

--- app/controllers/national_parks_controller.rb
+++ app/controllers/national_parks_controller.rb
@@ -1,7 +1,7 @@
 class NationalParksController < ApplicationController
 
   def index
-    national_parks = NationalPark.new
+    national_parks = NationalPark.all
     render json: national_parks.as_json
   end
 end
